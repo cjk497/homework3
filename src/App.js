e957c54cb23274e46f706d07bd63cab8591a5d56
Filip Bendevski <bende@localhost.localdomain>
2019-09-24 20:45:55 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -4,22 +4,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div class="container">
+        <h1>The Stack</h1>
+        <p>One of the negative impacts of a digital life that can be demonstrated through the Stack structure is the habit that many people have developed to "flex" on social media. More specifically, I've come to observe that social media users have developed a habit of doing novel things for the soul purpose of displaying them on social media. Whether it’s taking pictures of expensive food, videoing a whole concert, or posting vacation photos, many people forget to experience things physically and viscerally and are instead obsessed with having something to “flex” or display on their profiles for others to see and naturally, envy. There have been too many times where my view at a concert has been blocked by people glued to their phones whom are obsessed with whether or not their video of the concert turned out well enough to put on their Snapchat stories. Not only does that ruin my experience, but people obsessed with their phones also aren’t able to experience the fullness of something like a concert, which leads me to question whether or not the uniqueness of a concert experience is often lost because of people’s habit to “brag” on social media.</p>
+        <h2>User Layer</h2>
+        <img id="one" src="https://cel-fi.com/wp-content/uploads/2015/08/8.21.2015.jpg"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Concertgoers are the users that directly contribute to this loss in human connection at concerts, plays, and public events. Too often, people go to a concert only to film their time and thus only experiencing the concert through a lens, something they could do on their computers at home for free.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        
+        <h2>Interface Layer</h2>
+        <img id="two"src="https://www.wikihow.com/images/5/5e/Add-Multiple-Pictures-to-a-Snapchat-Story-Step-13-Version-3.jpg"/>
+        <p>
+            Social media platforms like Snapchat, Instagram, and Facebook contribute to the issue by providing a convenient way for people to document and share their lives and experiences.
+        </p>
+
+        <h2>Address Layer</h2>
+        <img id="three" src="https://cnet4.cbsistatic.com/img/KnuL1WDed3sanatLbE4YDddJGVg=/2017/10/31/312b3b6e-59b7-499a-aea4-9bc5f9721a21/iphone-x-54.jpg"/>
+        <p>
+            iPhones and other mobile devices that have the capability to film are often the culprits that enable people to film at concerts and public events.
+        </p>
+
+        <h2>City Layer</h2>
+        <img id="four" src="https://i-cdn.phonearena.com/images/article/103584-two_lead/Verizon-vs-AT-T-T-Mobile-and-Sprint-unlimited-data-plans-price-comparison.jpg"/>
+        <p>
+            Cellular providers like AT&T, Verizon, Sprint, T-Mobile and more make up most of the City Layer in this stack.
+        </p>
+
+        <h2>Cloud Layer</h2>
+        <img id="five"src="https://fbnewsroomus.files.wordpress.com/2015/05/lulea-fb-data-center-interior.jpg?w=960"/>
+        <p>
+            Data centers like these can often store the posts people choose to upload on their profiles. These data centers and servers thus act as the Cloud Layer.
+        </p>
+        
+        <h2>Earth Layer</h2>
+        <img id="six" src="http://www.asianjunkie.com/wp-content/uploads/2018/06/JisooPinkWig1.png"/>
+        <p>
+            Not only are the videos able to be shared internationally, but the effect of these videos are also worldwide. Even people across the world, like K-Pop group BLACKPINK, express their experiences with similar motifs around people's use of technology nowadays.
+        </p>
+        </div>
   );
 }
 
